564b017210cb2f22647d2d48dc4a173e6231b3b3
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-16 00:49:15 -0300

    fix

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -3,7 +3,11 @@ import { jwtValidator } from "../auth/jwtValidator";
 import { imageController } from "../controllers/imageController";
 
 export const router = express.Router();
-
+//Upload a image info on database
 router.post("/upload", jwtValidator, imageController.uploadImage);
+
+//get Images by user id
 router.get("/:id", jwtValidator, imageController.getImagesByUserId);
+
+//Delete image from imageId
 router.delete("/:id", jwtValidator, imageController.DeleteImageById);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,8 +6,18 @@ export const router = express.Router();
 
 //Register user on the database
 router.post("/register", userController.registerUser);
+
+//Login user
 router.post("/login", userController.loginUser);
+
+//Get all users
 router.get("/all", jwtValidator, userController.getAllUsers);
+
+//Get unique user
 router.get("/:id", jwtValidator, userController.getUserById);
+
+//Patch unique user
 router.patch("/:id", jwtValidator, userController.patchUserInfo);
+
+//Delete unique user
 router.delete("/:id", jwtValidator, userController.deleteUserById);
